dd968ef2a7462aad72d97921a65acb57b02b7a34
Ricardo Rodriguez <rr092293@gmail.com>
2018-12-12 14:13:37 -0800

    updates

--- index.js
+++ index.js
@@ -6,12 +6,12 @@ const methodOverride = require('method-override')
 
 const app = express()
 
-
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/rotten-potatoes');
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(methodOverride('_method'))
-mongoose.connect('mongodb://localhost/charity-tracker');
+// mongoose.connect('mongodb://localhost/charity-tracker');
 
 const charity = require('./controllers/charities')(app);
 
@@ -29,8 +29,9 @@ module.exports = app;
 // ]
 
 
+const port = process.env.PORT || 4000;
+app.listen(port);
 
-
-app.listen(4000, () => {
-  console.log('App listening on port 4000!')
-})
+// app.listen(4000, () => {
+//   console.log('App listening on port 4000!')
+// })
