9738cf1811843736d478d9442ff487e4aefff41d
Ricardo Rodriguez <rr092293@gmail.com>
2018-12-12 19:38:14 -0800

    fixes

--- index.js
+++ index.js
@@ -6,23 +6,13 @@ const charity = require('./controllers/charities')(app);
 const donation = require('./controllers/donations')(app);
 
 
-// const port = process.env.PORT || 4000;
+const port = process.env.PORT || 4000;
 
-mongoose.connect('mongodb://localhost/charity-tracker');
-//mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/rotten-potatoes');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/rotten-potatoes');
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
-// app.listen(port);
+app.listen(port);
 app.use(express.static('public'));
 
 
 module.exports = app;
-
-
-
-
-
-
-// app.listen(4000, () => {
-//   console.log('App listening on port 4000!')
-// })
